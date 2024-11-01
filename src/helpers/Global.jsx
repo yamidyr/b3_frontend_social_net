cab4b527f9e8f827ca0bb8738b68439260e68c7b
inesmariao <63691309+inesmariao@users.noreply.github.com>
2024-10-31 19:31:50 -0500

    configure variable Global into helpers

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,10 @@
+// Ruta del Backend desplegado
+export const Global = {
+  url: "https://b3-backend-social-net.onrender.com/api/"
+}
+
+
+// Ruta del Backend en Local
+// export const Global = {
+//   url: "http://localhost:3900/api/"
+// }
